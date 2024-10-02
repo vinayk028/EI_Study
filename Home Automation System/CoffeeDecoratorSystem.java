d9aa7545085209d9aaa12b511af5bbf098ac6559
Vinay Kumar <131520683+vinayk028@users.noreply.github.com>
2024-10-02 15:50:24 +0530

    Create CoffeeDecoratorSystem.java

---
+++ Home Automation System/CoffeeDecoratorSystem.java
@@ -0,0 +1,79 @@
+package com.patterns.structural.decorator;
+
+public class CoffeeDecoratorSystem {
+    public static void main(String[] args) {
+        Coffee basicCoffee = new SimpleCoffee();
+        System.out.println(basicCoffee.getDescription() + " $" + basicCoffee.cost());
+
+        Coffee milkCoffee = new MilkDecorator(new SimpleCoffee());
+        System.out.println(milkCoffee.getDescription() + " $" + milkCoffee.cost());
+
+        Coffee milkSugarCoffee = new SugarDecorator(new MilkDecorator(new SimpleCoffee()));
+        System.out.println(milkSugarCoffee.getDescription() + " $" + milkSugarCoffee.cost());
+    }
+}
+
+interface Coffee {
+    String getDescription();
+    double cost();
+}
+
+class SimpleCoffee implements Coffee {
+    @Override
+    public String getDescription() {
+        return "Simple Coffee";
+    }
+
+    @Override
+    public double cost() {
+        return 5.0;
+    }
+}
+
+abstract class CoffeeDecorator implements Coffee {
+    protected Coffee decoratedCoffee;
+
+    public CoffeeDecorator(Coffee coffee) {
+        this.decoratedCoffee = coffee;
+    }
+
+    public String getDescription() {
+        return decoratedCoffee.getDescription();
+    }
+
+    public double cost() {
+        return decoratedCoffee.cost();
+    }
+}
+
+class MilkDecorator extends CoffeeDecorator {
+    public MilkDecorator(Coffee coffee) {
+        super(coffee);
+    }
+
+    @Override
+    public String getDescription() {
+        return decoratedCoffee.getDescription() + ", Milk";
+    }
+
+    @Override
+    public double cost() {
+        return decoratedCoffee.cost() + 1.0;
+    }
+}
+
+class SugarDecorator extends CoffeeDecorator {
+    public SugarDecorator(Coffee coffee) {
+        super(coffee);
+    }
+
+    @Override
+    public String getDescription() {
+        return decoratedCoffee.getDescription() + ", Sugar";
+    }
+
+    @Override
+    public double cost() {
+        return decoratedCoffee.cost() + 0.5;
+    }
+}
